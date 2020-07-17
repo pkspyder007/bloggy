c23697c0a679cad184cf3c3fb3379e8d55ff84d0
pkpsyder007 <pkpsyder007@gmail.com>
2020-07-17 11:14:06 +0530

    adding build

---
+++ build/precache-manifest.470dddd2cf98742598e9a49adc14b0ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad5e125569815b9739c3d7054f65b2d",
+    "url": "/bloggy/index.html"
+  },
+  {
+    "revision": "437db6784847d51999b0",
+    "url": "/bloggy/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "3469470fa0a181b9a9eb",
+    "url": "/bloggy/static/css/main.68df3c9f.chunk.css"
+  },
+  {
+    "revision": "437db6784847d51999b0",
+    "url": "/bloggy/static/js/2.84a8a75c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bloggy/static/js/2.84a8a75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3469470fa0a181b9a9eb",
+    "url": "/bloggy/static/js/main.722ee6ec.chunk.js"
+  },
+  {
+    "revision": "53dd7f7358f3811fb951",
+    "url": "/bloggy/static/js/runtime-main.1a49acee.js"
+  },
+  {
+    "revision": "51be8bb8d8b815b533164c2a79855329",
+    "url": "/bloggy/static/media/hero1.51be8bb8.jpg"
+  }
+]);
